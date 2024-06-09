7c8e3eefa66829df2ec3e8a300f46a1d22ec731c
hardluker <dba9514@gmail.com>
2024-06-09 14:37:17 -0400

    Disabled debug mode

--- flightScraperAPI/settings.py
+++ flightScraperAPI/settings.py
@@ -23,8 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-bkh+1cdp3f%$*jx(rto*=ybr$(i=zo-ul0xw085o5nq8nsbt-#"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
+DEBUG = False
 ALLOWED_HOSTS = []
 
 
